75246e12f548a121cd587f2bc899587cd20e0067
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-09 10:35:20 -0600

    completed exercise 2 in JS intro

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
@@ -1,1 +1,10 @@
 // Write Your JavaScript Code Here
+let personName="Ladd";
+
+let numberOfPets="2"
+
+let funFact="he can burp on command"
+
+console.log("His name is " + personName)
+console.log("He has " + numberOfPets + " pets")
+console.log("His fun fact is " + funFact)
