a2095ca00a5ed01bbfc10c3d083ab167c87ae850
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-14 11:58:41 -0500

    completed exercise 1 on arrays

--- Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/01-Arrays/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/01-Arrays/Unsolved/script.js
@@ -1,1 +1,13 @@
 // WRITE YOUR CODE HERE
+let studentList = ["Hayden", "Ross", "Leland"];
+
+console.log(studentList.length);
+
+console.log(`Welcome to the class ${studentList[0]}, ${studentList[1]}, and ${studentList[2]}`);
+
+studentList[0]="Hunter";
+console.log(studentList[0])
+
+if (!studentList.includes("Hayden")){
+    console.log(`${studentList[0]} is in class!`)
+}
