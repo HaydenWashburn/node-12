8aacd5343b42ac152866cc1ab5c9e535474dbb50
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-22 09:28:47 -0500

    made a change on OOP exercise

--- Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
+++ Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
@@ -4,11 +4,11 @@ function Developer(name, tech){
     this.tech = tech;
 
     // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
-    introduction =() => {
+    this.introduction =() => {
     console.log(`This is ${this.name} and his favorite tech is ${this.tech}`)
     }
 }
 // TODO: Create a new object using the 'Developer' constructor
 const Hayden = new Developer("Hayden" , "computer")
 // TODO: Call the 'introduction()' method on the new object
-introduction(Hayden)
+Hayden.introduction()
