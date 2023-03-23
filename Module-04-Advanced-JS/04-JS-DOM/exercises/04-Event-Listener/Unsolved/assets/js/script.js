29b632fc0c8f7ef8ae05a3cb7fefccf252e50290
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-23 09:48:00 -0500

    finished exercise 4 event listener

--- Module-04-Advanced-JS/04-JS-DOM/exercises/04-Event-Listener/Unsolved/assets/js/script.js
+++ Module-04-Advanced-JS/04-JS-DOM/exercises/04-Event-Listener/Unsolved/assets/js/script.js
@@ -3,10 +3,21 @@ var incrementEl = document.querySelector("#increment");
 var decrementEl = document.querySelector("#decrement");
 var countEl = document.querySelector("#count");
 
-function setCounterText() {
-  countEl.textContent = count;
-}
 
+function setCounterText(){
+  if (this == incrementEl) {
+    count++;
+    countEl.textContent = count;
+  } else {
+    if (0 < count) {
+      count--;
+      countEl.textContent = count;
+    }
+  }
+}
 // TODO: Add event listener to increment button
+incrementEl.addEventListener("click", setCounterText)
 
 // TODO: Add event listener to decrement button 
+decrementEl.addEventListener("click" , setCounterText)
+
