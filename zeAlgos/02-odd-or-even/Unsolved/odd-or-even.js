96c01d7ac4499f6e7c7410e0db52efc95fb1a50d
HaydenWashburn <hayden.washburn1@gmail.com>
2023-04-10 09:04:40 -0500

    pulled down new changes

--- zeAlgos/02-odd-or-even/Unsolved/odd-or-even.js
+++ zeAlgos/02-odd-or-even/Unsolved/odd-or-even.js
@@ -1,4 +1,13 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
 
-var oddOrEven = function(num) {};
+var oddOrEven = function(num) {
+    if (num % 2 == 1){
+        return "odd"
+    }
+    else if (num %2 == 0 ){
+        return "even"
+    }
+};
+
+
--- zeAlgos/04-vowel-count/Unsolved/vowel-count.js
+++ zeAlgos/04-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,11 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+function vowelCount(str) {
+let count = 0;
+str.toLowerCase().split("").map((e) => {
+    if (e == "a" || e == "e" || e == "i" || e == "o" || e == "u"){
+        count++;
+    }
+});
+return count;
+}
